e2ddd126e1a016979421b2d8bb6aa8bf40ebef6c
Jaya Raj Srivathsav Adari <73651038+Srivathsav-max@users.noreply.github.com>
2025-05-07 13:16:10 -0400

    fix: enable multiple cors origin requests

--- backend/api/middleware.go
+++ backend/api/middleware.go
@@ -72,7 +72,7 @@ func AuthMiddleware(handler *Handler, cfg *config.Config) gin.HandlerFunc {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Update to use the frontend origin instead of wildcard
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, https://moxium.tech")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
