d3de690e11746805ec8a00e889805b7cbcb4439b
Jaya Raj Srivathsav Adari <73651038+Srivathsav-max@users.noreply.github.com>
2025-05-11 08:31:23 -0400

    fix: import fix

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import VerifyEmailClient from './emailClient';
+import VerifyEmailClient from './VerifyEmailClient';
 
 export default function VerifyEmailPage() {
   return (
